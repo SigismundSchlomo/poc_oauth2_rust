0164837453ecd03cac98814d2927fc853fff3ebf
Alex Kuznietsov <alex@kuznietsov.dev>
2021-11-28 13:00:37 +0200

    Some experiments

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
+use std::sync::Mutex;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    format!("Request number {}", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
+        // move counter into the closure
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            // Note: using app_data instead of data
+            .app_data(counter.clone()) // <- register the created data
+            .route("/", web::get().to(index))
     })
         .bind("127.0.0.1:8080")?
         .run()
